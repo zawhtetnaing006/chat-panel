1318b9f449bb95c2d710e218011d21f2c37df970
Zaw <zawhtetnaing006@gmail.com>
2024-05-08 17:40:51 +0700

    bugfix

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,6 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from 'class-validator';
 
 export class CreateUserDto {
-    nickname: string;
+  @IsString()
+  nickname: string;
 }
