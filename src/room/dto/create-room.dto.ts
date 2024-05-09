d12e2cb08f8f045670c17284d860db2207e4b170
Zaw <zawhtetnaing006@gmail.com>
2024-05-09 17:29:06 +0700

    [main] add create room and rooms model

--- src/room/dto/create-room.dto.ts
+++ src/room/dto/create-room.dto.ts
@@ -1,1 +1,4 @@
-export class CreateRoomDto {}
+export class CreateRoomDto {
+  name: string;
+  users: string[];
+}
--- src/room/room.service.ts
+++ src/room/room.service.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class RoomService {
+  constructor(private prisma: PrismaService) {}
   create(createRoomDto: CreateRoomDto) {
+    const data = {
+      name: createRoomDto.name,
+      users: {
+        create: createRoomDto.users.map((user) => {
+          return {
+            user: {
+              connect: {
+                id: user,
+              },
+            },
+          };
+        }),
+      },
+    };
+    return this.prisma.room.create({
+      data: data,
+      include: {
+        users: true,
+      },
+    });
     return 'This action adds a new room';
   }
 
