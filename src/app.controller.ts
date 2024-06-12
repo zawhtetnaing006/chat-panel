dd82a492b1591dee72a8883fa72a42ebe4e99f35
Zaw <zawhtetnaing006@gmail.com>
2024-06-12 13:46:10 +0700

    [main] add health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, SetMetadata } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('healthCheck')
+  @SetMetadata('isPublic', true)
   getHello(): string {
     return this.appService.getHello();
   }
--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -9,14 +9,25 @@ import { Request } from 'express';
 
 import { JsonWebTokenError, JwtService, TokenExpiredError } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
     private readonly jwtService: JwtService,
     private readonly userService: UserService,
+    private readonly reflector: Reflector,
   ) {}
   async canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.get<boolean>(
+      'isPublic',
+      context.getHandler(),
+    );
+
+    if (isPublic) {
+      return true;
+    }
+
     let isAuthenicated = false;
     const req = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(req);
